2a803e7171b531bdda76b001b499c8e8eefafff4
Chiel Kunkels <chiel@kunkels.me>
2022-12-15 13:35:22 +0100

    aelTodoDragonflightProfessions: add treatise quest IDs

---
+++ aelTodoDragonflightProfessions/init.lua
@@ -0,0 +1,45 @@
+aura_env.entriesByProfession = {
+	[171] = { -- alchemy
+		{ name = 'Treatise', quests = { 74108 } },
+	},
+
+	[164] = { -- blacksmithing
+		{ name = 'Treatise', quests = { 74109 } },
+	},
+
+	[333] = { -- enchanting
+		{ name = 'Treatise', quests = { 74110 } },
+	},
+
+	[202] = { -- engineering
+		{ name = 'Treatise', quests = { 74111 } },
+	},
+
+	[182] = { -- herbalism
+		{ name = 'Treatise', quests = { 74107 } },
+	},
+
+	[773] = { -- inscription
+		{ name = 'Treatise', quests = { 74105 } },
+	},
+
+	[755] = { -- jewelcrafting
+		{ name = 'Treatise', quests = { 74112 } },
+	},
+
+	[165] = { -- leatherworking
+		{ name = 'Treatise', quests = { 74113 } },
+	},
+
+	[186] = { -- mining
+		{ name = 'Treatise', quests = { 74106 } },
+	},
+
+	[393] = { -- skinning
+		{ name = 'Treatise', quests = { 74114 } },
+	},
+
+	[197] = { -- tailoring
+		{ name = 'Treatise', quests = { 74115 } },
+	},
+}
