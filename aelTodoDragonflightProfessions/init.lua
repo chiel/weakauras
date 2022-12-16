3f3b1dfa54b6e59f8e0fb87c742b438323993267
Chiel Kunkels <chiel@kunkels.me>
2022-12-16 16:18:20 +0100

    aelTodoDragonflightProfessions: add trigger + treatises

--- aelTodoDragonflightProfessions/init.lua
+++ aelTodoDragonflightProfessions/init.lua
@@ -1,45 +1,146 @@
+function hasCompleted(amount)
+	return function(quests, turnedInQuest)
+		if amount == 'ALL' then
+			amount = #quests
+		end
+
+		if amount == 'ANY' then
+			amount = 1
+		end
+
+		local completedCount = 0
+		for _, quest in ipairs(quests) do
+			local completed = turnedInQuest == quest or C_QuestLog.IsQuestFlaggedCompleted(quest)
+			if completed then
+				completedCount = completedCount + 1
+
+				if completedCount >= amount then
+					return true, completedCount, amount
+				end
+			end
+		end
+
+		return false, completedCount, amount
+	end
+end
+
+function hasSkill(requiredSkill)
+	return function(currentSkill)
+		return currentSkill >= requiredSkill
+	end
+end
+
 aura_env.entriesByProfession = {
 	[171] = { -- alchemy
-		{ name = 'Treatise', quests = { 74108 } },
+		{
+			name = 'Treatise',
+			quests = { 74108 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[164] = { -- blacksmithing
-		{ name = 'Treatise', quests = { 74109 } },
+		{
+			name = 'Treatise',
+			quests = { 74109 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[333] = { -- enchanting
-		{ name = 'Treatise', quests = { 74110 } },
+		{
+			name = 'Treatise',
+			quests = { 74110 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[202] = { -- engineering
-		{ name = 'Treatise', quests = { 74111 } },
+		{
+			name = 'Treatise',
+			quests = { 74111 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[182] = { -- herbalism
-		{ name = 'Treatise', quests = { 74107 } },
+		{
+			name = 'Treatise',
+			quests = { 74107 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[773] = { -- inscription
-		{ name = 'Treatise', quests = { 74105 } },
+		{
+			name = 'Treatise',
+			quests = { 74105 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[755] = { -- jewelcrafting
-		{ name = 'Treatise', quests = { 74112 } },
+		{
+			name = 'Treatise',
+			quests = { 74112 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[165] = { -- leatherworking
-		{ name = 'Treatise', quests = { 74113 } },
+		{
+			name = 'Treatise',
+			quests = { 74113 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[186] = { -- mining
-		{ name = 'Treatise', quests = { 74106 } },
+		{
+			name = 'Treatise',
+			quests = { 74106 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[393] = { -- skinning
-		{ name = 'Treatise', quests = { 74114 } },
+		{
+			name = 'Treatise',
+			quests = { 74114 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
 
 	[197] = { -- tailoring
-		{ name = 'Treatise', quests = { 74115 } },
+		{
+			name = 'Treatise',
+			quests = { 74115 },
+			isAvailable = hasSkill(25),
+			isCompleted = hasCompleted 'ANY',
+		},
 	},
+}
+
+aura_env.knowledgePointCurrencies = {
+	[2023] = true, -- blacksmithing
+	[2024] = true, -- alchemy
+	[2025] = true, -- leatherworking
+	[2026] = true, -- tailoring
+	[2027] = true, -- engineering
+	[2028] = true, -- inscription
+	[2029] = true, -- jewelcrafting
+	[2030] = true, -- enchanting
+	[2033] = true, -- skinning
+	[2034] = true, -- herbalism
+	[2035] = true, -- mining
 }
---
+++ aelTodoDragonflightProfessions/trigger.lua
@@ -0,0 +1,76 @@
+function Trigger(event, ...)
+	-- knowledge points are treated as currencies, there is no `QUEST_TURNED_IN` event
+	-- associated with using a treatise, nor is the hidden quest immediately marked as
+	-- completed by `IsQuestFlaggedCompleted`, so instead I set a timer for 1 second
+	-- and fire a fake event which re-triggers this function and causes a full update
+	if event == 'CURRENCY_DISPLAY_UPDATE' then
+		local currency = select(1, ...)
+		if aura_env.knowledgePointCurrencies[currency] then
+			C_Timer.After(1, function()
+				WeakAuras.ScanEvents 'AEL_TODO_DRAGONFLIGHT_PROFESSIONS_UPDATE'
+			end)
+		end
+
+		return
+	end
+
+	local turnedInQuest = nil
+	if event == 'QUEST_TURNED_IN' then
+		turnedInQuest = select(1, ...)
+	end
+
+	local profMap = {}
+	local profNames = {}
+	local prof1, prof2 = GetProfessions()
+	for _, profIndex in ipairs { prof1, prof2 } do
+		if profIndex == nil then
+			break
+		end
+
+		local name, icon, skillLevel, _, _, _, skillLine = GetProfessionInfo(profIndex)
+		profMap[name] = {
+			name = name,
+			icon = icon,
+			skillLevel = skillLevel,
+			skillLine = skillLine,
+		}
+		table.insert(profNames, name)
+	end
+
+	table.sort(profNames)
+
+	local sections = {}
+	for _, profName in ipairs(profNames) do
+		local prof = profMap[profName]
+		local rawEntries = aura_env.entriesByProfession[prof.skillLine]
+
+		local entries = {}
+		for _, entry in ipairs(rawEntries) do
+			local state = entry.isAvailable ~= nil and 'UNAVAILABLE' or 'PENDING'
+			if entry.isAvailable ~= nil and entry.isAvailable(prof.skillLevel) then
+				state = 'PENDING'
+			end
+
+			if entry.isCompleted(entry.quests, turnedInQuest) then
+				state = 'COMPLETED'
+			end
+
+			table.insert(entries, {
+				name = entry.name,
+				state = state,
+			})
+		end
+
+		if #entries > 0 then
+			table.insert(sections, { name = prof.name, entries = entries })
+		end
+	end
+
+	local plugin = {
+		name = 'Dragonflight Professions',
+		priority = 'MEDIUM',
+		sections = sections,
+	}
+
+	WeakAuras.ScanEvents('AEL_TODO_UPDATE', 'dragonflight_professions', plugin)
+end
