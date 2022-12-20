a17a37b6d1970377fb36f221570310e6ce7fe379
Chiel Kunkels <chiel@kunkels.me>
2022-12-20 02:29:11 +0100

    errands: add weekly dungeon quests

--- aelTodoDragonflightErrands/init.lua
+++ aelTodoDragonflightErrands/init.lua
@@ -30,18 +30,29 @@ aura_env.weeklies = {
 		quests = { 70750, 72068, 72373, 72374 },
 		isCompleted = hasCompleted 'ANY',
 	},
+	{
+		name = 'Dungeon Quests',
+		quests = { 66868, 66874, 66875 },
+		isCompleted = hasCompleted(2),
+	},
 }
 
 aura_env.processEntries = function(rawEntries, turnedInQuest)
 	local entries = {}
 	for _, entry in ipairs(rawEntries) do
 		local state = 'PENDING'
-		if entry.isCompleted(entry.quests, turnedInQuest) then
+		local completed, completedCount, totalCount = entry.isCompleted(entry.quests, turnedInQuest)
+		if completed then
 			state = 'COMPLETED'
 		end
 
+		local progress = ''
+		if totalCount > 1 then
+			progress = string.format(' (%d/%d)', completedCount, totalCount)
+		end
+
 		table.insert(entries, {
-			name = entry.name,
+			name = entry.name .. progress,
 			state = state,
 		})
 	end
