dc53ecb2a6a22fa46cfe3e0e71928a2f7c004064
Chiel Kunkels <chiel@kunkels.me>
2023-05-09 18:05:21 +0200

    zaralek: initial version

---
+++ aelTodoZaralekCavern/init.lua
@@ -0,0 +1,92 @@
+aura_env.patchRelease = time {
+	year = 2023,
+	month = 05,
+	day = 02,
+	hour = 6,
+	isdst = true,
+}
+
+aura_env.general = {
+	{
+		name = 'Briggul daily',
+		quests = { 74514, 74516 },
+		coords = '44.25 79.93',
+	},
+}
+
+aura_env.subzones = {
+	{
+		name = 'Caldera',
+		events = {
+			{ name = 'Mortar Warfare', quests = { 75454 }, coords = '40.41 42.95', note = 'weekly' },
+			{ name = 'Conspiracy of Flames', quests = { 75455 }, coords = '31.81 44.05', note = 'weekly' },
+			{ name = 'Imperfect Balance', quests = { 75451 }, coords = '35.13 51.63', note = 'weekly' },
+			{ name = 'Seismic Ceremony', quests = { 75450 }, coords = '34.50 48.07', note = 'weekly' },
+		},
+		rares = {
+			{ name = 'Dinn', quests = { 75352 }, coords = '28.33 51.30' },
+			{ name = 'Emberdusk', quests = { 75361 }, coords = '31.91 51.72' },
+			{ name = 'Flowfy', quests = { 75357 }, coords = '35.95 44.08' },
+			{ name = 'Skornak', quests = { 75348 }, coords = '36.18 53.83' },
+			{ name = 'Subterrax', quests = { 75359 }, coords = '38.22 46.85' },
+		},
+	},
+	{
+		name = 'Glimmerogg',
+		events = {
+			{ name = 'Cascades Calling', quests = { 75612 }, coords = '44.33 74.37' },
+			{ name = 'Glimmerfish Before It\'s Gone', quests = { 75611 }, coords = '39.52 79.16' },
+			{ name = 'The Champion\'s Challenge', quests = { 75624 }, coords = '????????' },
+		},
+		rares = {
+			{ name = 'Aquifon', quests = { 75270 }, coords = '48.35 74.91' },
+			{ name = 'Brullo the Strong', quests = { 75325 }, coords = '41.42 86.15' },
+			{ name = 'Karokta', quests = { 75333 }, coords = '42.19 65.36' },
+			{ name = 'Viridian King', quests = { 75365 }, coords = '39.02 70.58' },
+		},
+	},
+	{
+		name = 'Nal ks\'kol',
+		events = {
+			{ name = 'Discordant Crystals', quests = { 75664 }, coords = '56.61 65.71' },
+			{ name = 'Monument Maintenance', quests = { 75705 }, coords = '60.89 70.25' },
+			{ name = 'Whirling Zephyr', quests = { 74352 }, coords = '58.18 67.15' },
+		},
+		rares = {
+			{ name = 'Alcanon', quests = { 75284 }, coords = '56.23 73.74' },
+			{ name = 'Spinmarrow', quests = { 75275 }, coords = '53.19 64.70' },
+		},
+	},
+	{
+		name = 'Loamm',
+		events = {
+			{ name = 'Crystalline Survey', quests = { 75471 }, coords = '62.88 47.11' },
+			{ name = 'Shellfire', quests = { 75222 }, coords = '57.52 49.40' },
+			{ name = 'Smellincense', quests = { 75370 }, coords = '63.13 54.60' },
+			{ name = 'Smelly Scramble', quests = { 75441 }, coords = '57.30 57.30' },
+			{ name = 'Stress Express', quests = { 75156 }, coords = '????????' },
+		},
+		rares = {
+			{ name = 'Goopal', quests = { 75273 }, coords = '68.71 46.01' },
+			{ name = 'Kapraku', quests = { 75268 }, coords = '59.39 39.88' },
+			{ name = 'Klakatak', quests = { 75321 }, coords = '53.92 40.87' },
+			{ name = 'Kob\'rok', quests = { 75266 }, coords = '65.20 55.73' },
+			{ name = 'Underlight Queen', quests = { 75297 }, coords = '58.18 69.26' },
+		},
+	},
+	{
+		name = 'Aberrus',
+		events = {
+			{ name = 'Hungry Hungry Hydra', quests = { 75478 }, coords = '46.84 20.12' },
+			{ name = 'Magmaclaw Matriarch', quests = { 75461 }, coords = '47.19 25.04' },
+			{ name = 'Strike the Colors', quests = { 75494 }, coords = '????????' },
+		},
+		rares = {
+			{ name = 'Colossian', quests = { 75475 }, coords = '48.39 24.37' },
+			{ name = 'General Zskorro', quests = { 75295 }, coords = '42.22 18.72' },
+			{ name = 'Invoq ', quests = { 75335 }, coords = '45.75 33.21' },
+			{ name = 'Magtembo', quests = { 75339 }, coords = '41.49 38.31' },
+			{ name = 'Professor Gastrinax', quests = { 75291 }, coords = '55.23 19.41' },
+		},
+	},
+}
---
+++ aelTodoZaralekCavern/trigger.lua
@@ -0,0 +1,57 @@
+-- PLAYER_ENTERING_WORLD, PLAYER_TARGET_CHANGED, STATUS
+
+function Trigger(event, ...)
+	local turnedInQuest = nil
+	if event == 'QUEST_TURNED_IN' then
+		turnedInQuest = select(1, ...)
+	end
+
+	local inactiveZone = math.floor((GetServerTime() - aura_env.patchRelease) / 60 / 60 / 24 % 4) + 2
+
+	local events = {}
+	local rares = {}
+
+	for i, zone in ipairs(aura_env.subzones) do
+		if i ~= inactiveZone then
+			for _, entry in ipairs(zone.events) do
+				table.insert(events, entry)
+			end
+			for _, entry in ipairs(zone.rares) do
+				table.insert(rares, entry)
+			end
+		end
+	end
+
+	table.sort(events, function(a, b)
+		return a.name < b.name
+	end)
+
+	table.sort(rares, function(a, b)
+		return a.name < b.name
+	end)
+
+	local sections = {}
+
+	local entries = AelTodoProcessEntries(aura_env.general, turnedInQuest)
+	if #entries > 0 then
+		table.insert(sections, { name = 'Zaralek Cavern', entries = entries })
+	end
+
+	entries = AelTodoProcessEntries(rares, turnedInQuest)
+	if #entries > 0 then
+		table.insert(sections, { name = 'Zaralek Cavern Rares', entries = entries })
+	end
+
+	entries = AelTodoProcessEntries(events, turnedInQuest)
+	if #entries > 0 then
+		table.insert(sections, { name = 'Zaralek Cavern Events', entries = entries })
+	end
+
+	local plugin = {
+		name = 'Zaralek Cavern',
+		priority = 'LOW',
+		sections = sections,
+	}
+
+	WeakAuras.ScanEvents('AEL_TODO_UPDATE', 'zaralek_cavern', plugin)
+end
