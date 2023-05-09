70d683dfd11b861d382586afa4f731ed3bfd2e87
Chiel Kunkels <chiel@kunkels.me>
2023-05-09 18:04:35 +0200

    aelTodo: add some shared functions

--- aelTodo/init.lua
+++ aelTodo/init.lua
@@ -15,3 +15,61 @@ aura_env.stateColors = {
 	PENDING = aura_env.colors.pending,
 	UNAVAILABLE = aura_env.colors.unavailable,
 }
+
+function AelTodoHasCompleted(amount)
+	return function(quests, turnedInQuest)
+		if amount == 'ALL' then
+			amount = #quests
+		end
+
+		if amount == 'ANY' then
+			amount = 1
+		end
+
+		local completedCount = 0
+		for _, quest in ipairs(quests) do
+			local completed = turnedInQuest == quest or C_QuestLog.IsQuestFlaggedCompleted(quest)
+			if completed then
+				completedCount = completedCount + 1
+
+				if completedCount >= amount then
+					return true, completedCount, amount
+				end
+			end
+		end
+
+		return false, completedCount, amount
+	end
+end
+
+local defaultIsCompleted = hasCompleted 'ANY'
+
+function AelTodoProcessEntries(rawEntries, turnedInQuest)
+	local entries = {}
+	for _, entry in ipairs(rawEntries) do
+		local state = entry.isAvailable ~= nil and 'UNAVAILABLE' or 'PENDING'
+		if entry.isAvailable ~= nil and entry.isAvailable() then
+			state = 'PENDING'
+		end
+
+		local isCompleted = entry.isCompleted or defaultIsCompleted
+		local completed, completedCount, totalCount = isCompleted(entry.quests, turnedInQuest)
+		if completed then
+			state = 'COMPLETED'
+		end
+
+		local progress = ''
+		if state ~= 'UNAVAILABLE' and totalCount > 1 then
+			progress = string.format(' (%d/%d)', completedCount, totalCount)
+		end
+
+		table.insert(entries, {
+			coords = entry.coords,
+			name = entry.name .. progress,
+			note = entry.note,
+			state = state,
+		})
+	end
+
+	return entries
+end
